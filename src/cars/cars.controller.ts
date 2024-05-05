5246c5d298fa507d0593d461ba94bd90fb8fe42e
Ildefonso Valenzuela <ponchovalo@outlook.com>
2024-05-04 20:43:18 -0600

    may04

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,33 +1,38 @@
-import { Body, Controller, Delete, Get, MethodNotAllowedException, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, MethodNotAllowedException, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { CreateCarDto } from './dto/create-car.dto';
+import { UpdateCarDto } from './dto/update-car.dto';
 
 @Controller('cars')
 export class CarsController {
 
     constructor( private carsService: CarsService) {}
 
+    //Obtiene todos los datos de la lista de carros
     @Get()
     getAllCars(){
         return this.carsService.findAll();
     }
 
+    //metod que obtiene un solo elemento de la lista de carro con un id dado
     @Get(':id')
-    getById(@Param('id', ParseIntPipe ) id: number ){
+    getById(@Param('id', new ParseUUIDPipe({version: '4'}) ) id: string ){
         return this.carsService.findOneById(id);
     }
+
     // Metodo post para la creacion de un elemento del arreglo de carros
     @Post()
-    create( @Body() body: any) {
-        return body 
+    create( @Body() createCarDto: CreateCarDto) {
+        return this.carsService.create(createCarDto);
     }
 
     //Metodo para la edicion de un elemento del arreglo de carros
     @Patch(':id')
     updateCar( 
-        @Param('id', ParseIntPipe) id: number, 
-        @Body() body: any ) 
+        @Param('id', new ParseUUIDPipe({version: '4'})) id: string, 
+        @Body() updateCarDto: UpdateCarDto ) 
         {
-            return body;
+            return this.carsService.update(id, updateCarDto);
         }
 
     //Metodo para la eliminacion de un elemento del arreglo de carros
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,36 +1,62 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { Car } from './interfaces/car.interface';
+import { v4 as uuid } from 'uuid'
+import { CreateCarDto, UpdateCarDto } from './dto';
 
 @Injectable()
 export class CarsService {
 
-    private cars = [ 
+    private cars: Car[] = [ 
         {
-            id: 1,
+            id: uuid(),
             brand: 'Toyota',
             model: 'Corolla'
         },
         {
-            id: 2,
+            id: uuid(),
             brand: 'Honda',
             model: 'Civic'
         },
         {
-            id: 3,
+            id: uuid(),
             brand: 'Jeep',
             model: 'Cheroke'
         }
     ];
 
-
     findAll(){
         return this.cars
     }
 
-    findOneById(id: number){
+    findOneById(id: string){
         const car = this.cars.find( car => car.id === id);
         if(!car) throw new NotFoundException(`Car with '${id}' not found`);
         return car 
     }
+
+    create( createCarDto: CreateCarDto){
+        const car: Car = {
+            id: uuid(),
+            ...createCarDto
+        };
+        this.cars.push(car);
+        return car;
+    }
+
+    update(id: string, updateCarDto: UpdateCarDto){
+
+        let carDB = this.findOneById(id);
+
+        this.cars = this.cars.map(car => {
+            if(car.id === id){
+                carDB = {...carDB, ...updateCarDto,id }
+                return carDB
+            }
+            return car
+        })
+        return carDB
+    }
+
 
 
 
